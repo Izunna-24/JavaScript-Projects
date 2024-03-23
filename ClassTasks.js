a46c2bd4bae7a7647548bad8057bc748a8208b36
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-23 02:31:47 +0100

    JS

---
+++ ClassTasks.js
@@ -0,0 +1,11 @@
+var1 = 2;
+var2 = var1;
+var3 = var2;
+ 
+console.log("\n var1 = " + var1 +" \n var2 = " + var2 +"\n var3 = " +var3);
+
+var1 = var2;
+ var2 = var3;
+ var3 = var1;
+
+ console.log("\n var1 = " + var1 +" \n var2 = " + var2 +"\n var3 = " +var3);
